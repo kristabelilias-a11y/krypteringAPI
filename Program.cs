639671b9efda5837559000e7b6205e9ee3ade8c6
Kristabel Ilias <kristabel.ilias@hotmail.com>
2026-02-09 14:11:31 +0100

    tagit bort cryptocontroller

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddMvcCore()
-    .AddApiExplorer()
-    .AddApplicationPart(typeof(CryptoController).Assembly)
-    .AddControllersAsServices();
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,8 +10,5 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.Run();
